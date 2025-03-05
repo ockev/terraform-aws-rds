e349bf797846763717b128717d2579a1c60c9d4a
Kevin O'Connor <kevinoconnor@kevinoconnor-f7q5mv9gpx.lan>
2025-03-05 08:46:10 -0500

    removed VPC module and added VPC components locally

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+# main.tf
 
 terraform {
   required_providers {
@@ -12,10 +11,9 @@ terraform {
       version = "~> 3.5"
     }
   }
-}
-
   # optional but recommended:
   # required_version = ">= 1.3.0"
+}
 
 provider "aws" {
   region = "us-east-1"
@@ -27,30 +25,123 @@ data "aws_availability_zones" "available" {}
 
 resource "random_pet" "random" {}
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
-
-  name                 = "${random_pet.random.id}-education"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_dns_hostnames = true
+# ------------------------------------------------------------------------------
+# 1) VPC
+# ------------------------------------------------------------------------------
+resource "aws_vpc" "education" {
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
-}
-
-resource "aws_db_subnet_group" "education" {
-  name       = "${random_pet.random.id}-education"
-  subnet_ids = module.vpc.public_subnets
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "${random_pet.random.id} Education"
+    Name = "${random_pet.random.id}-education-vpc"
   }
 }
 
+# ------------------------------------------------------------------------------
+# 2) Public subnets
+#    We'll place them in the first three AZs from data.aws_availability_zones.
+# ------------------------------------------------------------------------------
+resource "aws_subnet" "education_public_az1" {
+  vpc_id                  = aws_vpc.education.id
+  cidr_block             = "10.0.4.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${random_pet.random.id}-az1"
+  }
+}
+
+resource "aws_subnet" "education_public_az2" {
+  vpc_id                  = aws_vpc.education.id
+  cidr_block             = "10.0.5.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${random_pet.random.id}-az2"
+  }
+}
+
+resource "aws_subnet" "education_public_az3" {
+  vpc_id                  = aws_vpc.education.id
+  cidr_block             = "10.0.6.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[2]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${random_pet.random.id}-az3"
+  }
+}
+
+# ------------------------------------------------------------------------------
+# 3) Internet Gateway
+# ------------------------------------------------------------------------------
+resource "aws_internet_gateway" "education" {
+  vpc_id = aws_vpc.education.id
+
+  tags = {
+    Name = "${random_pet.random.id}-education-igw"
+  }
+}
+
+# ------------------------------------------------------------------------------
+# 4) Public Route Table + routes
+# ------------------------------------------------------------------------------
+resource "aws_route_table" "education_public" {
+  vpc_id = aws_vpc.education.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.education.id
+  }
+
+  tags = {
+    Name = "${random_pet.random.id}-education-public-rtb"
+  }
+}
+
+# ------------------------------------------------------------------------------
+# 5) Route table associations for each public subnet
+# ------------------------------------------------------------------------------
+resource "aws_route_table_association" "public_az1" {
+  subnet_id      = aws_subnet.education_public_az1.id
+  route_table_id = aws_route_table.education_public.id
+}
+
+resource "aws_route_table_association" "public_az2" {
+  subnet_id      = aws_subnet.education_public_az2.id
+  route_table_id = aws_route_table.education_public.id
+}
+
+resource "aws_route_table_association" "public_az3" {
+  subnet_id      = aws_subnet.education_public_az3.id
+  route_table_id = aws_route_table.education_public.id
+}
+
+# ------------------------------------------------------------------------------
+# 6) RDS Subnet Group (use the 3 public subnets)
+# ------------------------------------------------------------------------------
+resource "aws_db_subnet_group" "education" {
+  name       = "${random_pet.random.id}-education"
+  subnet_ids = [
+    aws_subnet.education_public_az1.id,
+    aws_subnet.education_public_az2.id,
+    aws_subnet.education_public_az3.id,
+  ]
+
+  tags = {
+    Name = "${random_pet.random.id}-education"
+  }
+}
+
+# ------------------------------------------------------------------------------
+# 7) Security Group for RDS
+# ------------------------------------------------------------------------------
 resource "aws_security_group" "rds" {
   name   = "${random_pet.random.id}-education_rds"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = aws_vpc.education.id
 
   ingress {
     from_port   = 5432
@@ -71,6 +162,9 @@ resource "aws_security_group" "rds" {
   }
 }
 
+# ------------------------------------------------------------------------------
+# 8) Optional Parameter Group
+# ------------------------------------------------------------------------------
 resource "aws_db_parameter_group" "education" {
   name   = "${random_pet.random.id}-education"
   family = "postgres15"
@@ -81,11 +175,15 @@ resource "aws_db_parameter_group" "education" {
   }
 }
 
+# ------------------------------------------------------------------------------
+# 9) RDS Instance
+# ------------------------------------------------------------------------------
 resource "aws_db_instance" "education" {
   identifier             = "${var.db_name}-${random_pet.random.id}"
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
+  engine_version         = "15.7"
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.education.name
@@ -93,4 +191,8 @@ resource "aws_db_instance" "education" {
   parameter_group_name   = aws_db_parameter_group.education.name
   publicly_accessible    = true
   skip_final_snapshot    = true
+
+  tags = {
+    Name = "${var.db_name}-${random_pet.random.id}"
+  }
 }
